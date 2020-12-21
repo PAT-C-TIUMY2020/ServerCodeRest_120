a0d30b60531dd18364538ae71a5d6627ea1739ae
Zulqoniun <muhammadqoni123@gmail.com>
2020-12-21 20:07:36 +0700

    Add project files.

---
+++ ServerCodeRest_20180140120_MuhammadZulqoniun/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using ServiceRest_20180140120_MuhammadZulqoniun;
+using System.ServiceModel.Description;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Web;
+
+namespace ServerCodeRest_20180140120_MuhammadZulqoniun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+            Uri address = new Uri("http://localhost:1907/Mahasiswa");
+            WebHttpBinding bind = new WebHttpBinding();
+            try
+            {
+                hostObjek = new ServiceHost(typeof(TI_UMY), address);
+                //ALAMAT BASE ADDRESS
+                hostObjek.AddServiceEndpoint(typeof(ITI_UMY), bind, "");
+
+
+                //ALAMAT ENDPOINT
+                //wsdl
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior(); //Service Runtime Player
+                smb.HttpGetEnabled = true; //untuk mengaktifkan wsdl (dibuka saat development, tidak untuk dibuka)
+                hostObjek.Description.Behaviors.Add(smb);
+                //mex
+                Binding mexbind = MetadataExchangeBindings.CreateMexHttpBinding();
+                hostObjek.AddServiceEndpoint(typeof(IMetadataExchange), mexbind, "mex");
+
+                WebHttpBehavior whb = new WebHttpBehavior();
+                whb.HelpEnabled = true;
+                hostObjek.Description.Endpoints[0].EndpointBehaviors.Add(whb);
+
+                hostObjek.Open();
+                Console.WriteLine("Server is ready!!!!");
+                Console.ReadLine();
+                hostObjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
